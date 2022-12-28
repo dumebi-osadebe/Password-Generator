e875c4ce73d5b61ca931b89bbbc3e8991d156f4d
Dumebi Winifred <winifred2412@gmail.com>
2022-12-28 17:56:50 -0500

    Update to requirements function
    Updated requirements method to return the user input values

--- Main.java
+++ Main.java
@@ -6,17 +6,16 @@ public class Main{
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args){
            
-        getRequirements();
+        String[] requirements = getRequirements();
         
     }
 
-    static void getRequirements() {
-
+    public static String[] getRequirements() {
+        
+        String capitalization = "N";
         
         System.out.println("Length of password:");
-        int passLength = scan.nextInt(); 
-        scan.nextLine();
-        System.out.println("Username is: " + passLength);
+        String passLength = scan.nextLine();
         
         System.out.println("\nChoose from options below (enter A, B or C):");
         System.out.println(" A. Only Letters \n B. Only Numbers \n C. Letters and Numbers");
@@ -27,10 +26,13 @@ public class Main{
 
             System.out.println("\nChoose capitalization from options below (enter A, B or C):");
             System.out.println(" A. All Uppercase \n B. All Lowercase \n C. Uppercase and Lowercase");
-            String capitalization = scan.nextLine();
+            capitalization = scan.nextLine();
             System.out.println("Username is: " + capitalization);
 
         }
+        
+        String[] arr = {passLength, alphNum, capitalization};
+        return arr;
 
     }
 }
