89171536e133b569c49dd9a66d9185b4f41e4d93
Dumebi Osadebe <winifred2412@gmail.com>
2022-12-28 19:40:17 -0500

    password generation funtion

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 
 import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class Main{
     
     static Scanner scan = new Scanner(System.in);
+    static Random random = new Random();
     public static void main(String[] args){
            
         String[] requirements = getRequirements();
+        String password = generator(requirements[0], requirements[1], requirements[2]);
         
     }
 
@@ -35,4 +39,42 @@ public class Main{
         return arr;
 
     }
+
+    public static String generator(String passLength, String alphNum, String capitalization){
+
+        int length = Integer.parseInt(passLength);
+        ArrayList<Integer> randomNums = new ArrayList<>();
+        ArrayList<Character> randomVars = new ArrayList<>();
+        int upperbound = 9;
+        String password = "no";
+
+        //char data[] = {'a', 'b', 'c'};
+        //String str = new String(data);
+
+        for(int i = 0; i < length; i++){
+
+            // generate only e=random numbers for the password as requested by user input
+            if(alphNum.equals("B")){
+                int int_random = random.nextInt(upperbound);
+                randomNums.add(int_random);
+
+            } else if(alphNum.equals("A") && capitalization.equals("A")) {
+                char alphabet = (char)(random.nextInt(26) + 'A');
+                randomVars.add(alphabet);
+                System.out.println(randomVars.get(0));
+
+            }   else if(alphNum.equals("A") && capitalization.equals("B")) {
+                char alphabet = (char)(random.nextInt(26) + 'a');
+                randomVars.add(alphabet);
+                System.out.println(randomVars.get(0));
+
+            }
+
+    
+        }
+
+        return password;
+
+    }
+
 }
